c1984932a64d9fc0dc333c16f984834e99d03fac
Ihor Yermolenko <ermolenkoigor07@gmail.com>
2024-05-06 21:59:59 +0300

    Third commit

---
+++ js/task-1.js
@@ -0,0 +1,22 @@
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//     const allPrice = quantity * pricePerDroid;
+//     if (allPrice > customerCredits) {
+//         return "Insufficient funds!"; 
+//     } else {
+//         return `You ordered ${quantity} droids worth ${allPrice} credits!`;
+//     }
+// }
+
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+
+const arrayFirst = ['one', 'two', 'three'];
+console.log(arrayFirst[0]);
+
+console.log(arrayFirst[1]);
+
+console.log(arrayFirst[2]);
